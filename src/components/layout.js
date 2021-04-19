6e37310e68064d408732130a0561217d1d4e5b2c
vitaliyrtest1 <51633367+vitaliyrtest1@users.noreply.github.com>
2021-04-19 16:06:44 +0300

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-console.log('yes2');
+console.log('yes3');
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
